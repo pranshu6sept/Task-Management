477e826b941e3fe0cdc7d300cec29d3c79402d41
pranshu6sept <128319537+pranshu6sept@users.noreply.github.com>
2024-08-02 02:37:31 +0530

    error resolved

--- task-management-app/src/middleware.ts
+++ task-management-app/src/middleware.ts
@@ -21,8 +21,5 @@ export async function middleware(request: NextRequest) {
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
-    '/sign-in',
-    '/sign-up',
-    '/',
     ],
 }
